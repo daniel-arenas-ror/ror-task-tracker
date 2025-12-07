9ffb440a95927ac18463085ccc32b8da6793aa4f
Daniel Arenas <daniel1144151@gmail.com>
2025-12-07 14:37:59 +0000

    add task method

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::TasksController < ApplicationController
+
+  def create
+    task = Task.new(task_params)
+    if task.save
+      render json: task, status: :created
+    else
+      render json: { errors: task.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
+  end
 end
