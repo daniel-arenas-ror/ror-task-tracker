e2284750f5eca368abd611d503c0d3a6d99afd3b
Daniel Arenas <daniel1144151@gmail.com>
2025-12-07 14:55:10 +0000

    add test to index

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Tasks", type: :request do
+  describe "GET /api/v1/tasks" do
+    it "returns a list of tasks" do
+      create_list(:task, 3)
+
+      get api_v1_tasks_path
+
+      expect(response).to have_http_status(:ok)
+      json_response = JSON.parse(response.body)
+      expect(json_response.length).to eq(3)
+    end
+
+  end
+
   describe "POST /api/v1/tasks" do
     context "with valid parameters" do
-      let(:valid_attributes) { { task: attributes_for(:task, description: "Factory Generated Task") } }
+      let(:valid_attributes) { { task: attributes_for(:task, description: Faker::Lorem.sentence) } }
 
       it "creates a new Task" do
         expect {
