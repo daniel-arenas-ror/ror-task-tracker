3a224fd53b55242a0b8d6e852156ac2ad10bf851
Daniel Arenas <daniel1144151@gmail.com>
2025-12-07 15:00:25 +0000

    add order on task

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Tasks", type: :request do
   describe "GET /api/v1/tasks" do
-    it "returns a list of tasks" do
-      create_list(:task, 3)
+    let!(:task_1_older) { create(:task, created_at: 2.days.ago) }
+    let!(:task_2_middle) { create(:task, created_at: 1.days.ago) }
+    let!(:task_3_new) { create(:task) }
 
+    it "returns the correct number of tasks" do
       get api_v1_tasks_path
 
       expect(response).to have_http_status(:ok)
@@ -12,6 +14,18 @@ RSpec.describe "Api::V1::Tasks", type: :request do
       expect(json_response.length).to eq(3)
     end
 
+
+    it "returns tasks sorted by created_at descending (newest first)" do
+      get api_v1_tasks_path
+
+      expect(response).to have_http_status(:ok)
+      json_response = JSON.parse(response.body)
+
+      expected_ids_in_order = [task_3_new.id, task_2_middle.id, task_1_older.id]
+      actual_ids_in_order = json_response.map { |task| task['id'] }
+
+      expect(actual_ids_in_order).to eq(expected_ids_in_order)
+    end
   end
 
   describe "POST /api/v1/tasks" do
